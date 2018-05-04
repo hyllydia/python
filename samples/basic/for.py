9d70a40261d3c9519803166852549aba6c202109
02 <hyllydia@163.com>
2018-05-04 18:11:19 +0800

    Create for.py

---
+++ samples/basic/for.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+sum=0
+for x in range(101):
+   sum=sum+x
+   print(sum)
+
+names=['lydia','lucy','jenny']
+for i in names:
+   print(i)
+
+a=3
+i=0
+L=['Bart','Lisa','Adam']
+while a>0:
+   a=a-1
+   print('hello',L[i])
+   i=i+1
