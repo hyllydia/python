bdbc964cee112eb13f762b7ec65f851592ead1b1
02 <hyllydia@163.com>
2018-05-04 18:14:00 +0800

    Create the_dict.py

---
+++ samples/basic/the_dict.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+d={
+   'Lydia':95,
+   'Bob':75,
+   'Tracy':85
+}
+print('d[\'Lydia\']',d['Lydia'])
+print('d[\'Bob\']',d['Bob'])
+print('d[\'Tracy\']',d['Tracy'])
+print('d.get(\'Tom\',-1)=',d.get('Tom',-1))
