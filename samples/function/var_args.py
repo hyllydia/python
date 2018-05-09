d6d97ebb37db949b84cb56bd03af704aa37f6356
02 <hyllydia@163.com>
2018-05-09 11:08:04 +0800

    Update var_args.py

--- samples/function/var_args.py
+++ samples/function/var_args.py
@@ -15,7 +15,16 @@ def hello(greeting,*args):
    if (len(args)==0):
       print('%s!' %greeting)
    else:
-      print('%s!,%s' %(greeting,','.join(args)))  # .join() ？？？
+      print('%s!,%s' %(greeting,','.join(args))) 
+      '''
+      .join() 方法用于将序列中的元素以指定的字符连接生成一个新的字符串
+       a = ['a','b','c','d','e']
+       print '-'.join(a)
+       print '*'.join(a[1:3])
+       输出:
+       a-b-c-d-e
+       b*c
+       '''
 hello('Hi')   # =>greeting='HI', agrs()
 hello('Hi','Lydia') # => greeting='Hi', args=('Lydia')
 hello('Hi','Lydia','kelly','sandy') # =>greeting='Hi',agrs=('Lydia','kelly','sandy')
